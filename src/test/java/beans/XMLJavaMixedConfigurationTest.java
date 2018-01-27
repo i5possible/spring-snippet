97ff435f8c87a68d077feec912e88cfa83098ba5
Liang Hong <lianghongbuaa@gmail.com>
2018-01-27 14:23:28 +0800

    add xml java mixed configuration test. the idea get confused about this configuration

---
+++ src/test/java/beans/XMLJavaMixedConfigurationTest.java
@@ -0,0 +1,37 @@
+package beans;
+
+import beans.always.BasicBean;
+import beans.auto.MagicBean;
+import beans.other.AwesomeBean;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author lianghong
+ * @date 27/01/2018
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:application-mixed.xml")
+public class XMLJavaMixedConfigurationTest {
+    @Autowired
+    private BasicBean basicBean;
+
+    @Autowired
+    private MagicBean magicBean;
+
+    @Autowired
+    private AwesomeBean awesomeBean;
+
+    @Test
+    public void java_bean_configuration() {
+        assertNotNull(basicBean);
+        assertNotNull(magicBean);
+        assertNotNull(awesomeBean);
+    }
+}
