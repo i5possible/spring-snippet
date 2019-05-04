06ffbae48ba4add5fd243b81431aa91c23a36aa7
LiangHong_KY <lianghong@kyeegroup.com>
2019-05-04 21:59:10 +0800

    add serialization proxy pattern example

---
+++ src/main/java/serialization/SerializationProxy.java
@@ -0,0 +1,29 @@
+package serialization;
+
+import java.io.Serializable;
+import java.util.EnumSet;
+
+/**
+ * @author lianghong
+ * @date 2019/5/4
+ */
+
+//public class SerializationProxy<E extends Enum<E>> implements Serializable{
+//    private final Class<E> elementType;
+//    private final Enum[] elements;
+//
+//    SerializationProxy(EnumSet<E> set) {
+//        elementType = set.elementType;
+//        elements = set.toArray(EMPTY_ENUM_ARRAY);
+//    }
+//
+//    private Object readResolve() {
+//        EnumSet<E> result = EnumSet.noneOf(elementType);
+//        for (Enum e: elements) {
+//            result.add((E)e);
+//        }
+//        return result;
+//    }
+//
+//    private static final long serialVersionUID = -1;
+//}
