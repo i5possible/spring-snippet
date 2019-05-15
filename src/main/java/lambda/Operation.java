807f91205c24e195a1f2d493d2947ee2d33f1da2
LiangHong_KY <lianghong@kyeegroup.com>
2019-05-15 21:40:07 +0800

    add fromString method

--- src/main/java/lambda/Operation.java
+++ src/main/java/lambda/Operation.java
@@ -1,11 +1,14 @@
 package lambda;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author lianghong
  * @date 2019/5/1
  */
 
-public enum Operation {
+public enum Operation implements Comparable<Operation>{
     PLUS("+") {
         @Override
         public double apply(double x, double y) {
@@ -21,14 +24,27 @@ public enum Operation {
 
     ;
     private final String symbol;
+    private static final Map<String, Operation> stringToEnumMap
+        = new HashMap<>();
 
     Operation(String symbol) {
         this.symbol = symbol;
     }
 
+
+    static {
+        for (Operation operation : Operation.values()) {
+            stringToEnumMap.put(operation.toString(), operation);
+        }
+    }
+
     @Override
     public String toString() {
         return symbol;
+    }
+
+    public Operation fromString(String symbol) {
+        return stringToEnumMap.get(toString());
     }
 
     public abstract double apply(double x, double y);
